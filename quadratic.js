c02a266f469348033d85c93b7621c22c32dfedbe
jwebs22 <jenny.webster@codeimmersives.com>
2022-09-14 21:32:49 -0400

    Update quadratic.js

--- quadratic.js
+++ quadratic.js
@@ -1,9 +1,12 @@
-//not done. still working on this.
-
 let a = 2
 let b = 10
 let c = 5
 
-let sqForm = (b ** 2) - (4*a*c);
+let discriminate = b ** 2 - 4 * a * c;
 
-console.log();
+let x1 = (-b + (Math.sqrt(discriminate))) / (2 * a);
+let x2 = (-b - (Math.sqrt(discriminate))) / (2 * a);
+
+
+console.log(`When solving for X using the quadratic formula with a=2, b=10, and c=5 as variables, the solutions are ${x1} and ${x2}.`);
+
